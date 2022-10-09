e88252e61e865bf9a0296e57d9e02fdecddd0f0c
kennie <kennie747@gmail.com>
2022-10-09 20:31:35 +0100

    updated apiHost url

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://ad9209de587344a01af76160a00f98d2-410549248.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
