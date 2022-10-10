ab952faa5711668f0b6b443e894c8e3201d1f1db
kennie <kennie747@gmail.com>
2022-10-10 10:39:13 +0100

    update nginx and frontendapi

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://ad9209de587344a01af76160a00f98d2-410549248.us-east-1.elb.amazonaws.com:8080/api/v0'
+  apiHost: 'http://a5b517455fc9b4f8f99dcf10495594e7-1365828482.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
